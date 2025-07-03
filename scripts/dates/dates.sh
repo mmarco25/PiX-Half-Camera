13d2bd431cb9718714f181e08752c2412451e369
mmarco25 <mmarco25@zohomail.eu>
2025-07-03 12:37:47 +0100

    Edited date script Liquid Crystal Font

--- scripts/dates/dates.sh
+++ scripts/dates/dates.sh
@@ -1,4 +1,4 @@
    #!/bin/bash
    for img in *.JPG; do
-       convert "$img" -gravity NorthEast -pointsize 44 -fill red -annotate +130+130 "%[exif:DateTimeOriginal]" "time_"$img
+       convert "$img" -font LiquidCrystal-Regular -gravity NorthEast -pointsize 50 -fill orange -annotate +130+130 "%[exif:DateTimeOriginal]" "time_"$img
    done
